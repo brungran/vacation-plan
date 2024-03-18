18a6b70410702974079262960babfeaad61ed51a
brungran <brungran@gmail.com>
2024-03-18 16:44:28 -0300

    Wrote tests for each API endpoint.

--- tests/Feature/VacationPlanTest.php
+++ tests/Feature/VacationPlanTest.php
@@ -6,32 +6,32 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use Spatie\LaravelPdf\Facades\Pdf;
+use Spatie\LaravelPdf\PdfBuilder;
+use App\Models\VacationPlan;
 
 class VacationPlanTest extends TestCase
 {
     use RefreshDatabase;
-    private function create_personal_client(): void{
+
+    /* protected function setUp(): void{
+        parent::setUp();
+
+        $this->artisan('passport:client', [
+            '--personal'=>true
+        ])->expectsQuestion('What should we name the personal access client?', 'testing');
+    } */
+
+    private function create_passport_personal_client(): void{
         $this->artisan('passport:client', [
             '--personal'=>true
         ])->expectsQuestion('What should we name the personal access client?', 'testing');
     }
 
-    private function api_registration_endpoint_request(){
-
-        return $this->post('/api/register', [
-            'name'=>'user1',
-            'email'=>'user1@user1.com',
-            'password'=>'123456',
-            'password_confirmation'=>'123456'
-        ]);
-    }
-
-    private function api_login_endpoint_request(){
-
-        return $this->post('/api/login', [
-            'email' => 'user1@user1.com',
-            'password' => '123456',
-        ]);
+    private function create_user_with_token(){
+        $user = User::factory()->create();
+        $token = $user->createToken('token');
+        return $token;
     }
 
     /**
@@ -39,7 +39,12 @@ class VacationPlanTest extends TestCase
      */
     public function test_api_registration(): void
     {
-        $response = $this->api_registration_endpoint_request();
+        $response = $this->post('/api/register', [
+            'name'=>'user1',
+            'email'=>'user1@user1.com',
+            'password'=>'123456',
+            'password_confirmation'=>'123456'
+        ]);
 
         $response->assertStatus(201);
         $response->assertJson([
@@ -53,10 +58,19 @@ class VacationPlanTest extends TestCase
     public function test_api_login(): void
     {
         
-        $this->create_personal_client();
-        $this->api_registration_endpoint_request();
+        $this->create_passport_personal_client();
         
-        $response = $this->api_login_endpoint_request();
+        User::create([
+            'name'=> 'name 1',
+            'email'=> 'user1@user1.com',
+            'password' => '123456',
+            'password_confirmation'=>'123456'
+        ]);
+        
+        $response = $this->post('/api/login', [
+            'email' => 'user1@user1.com',
+            'password' => '123456',
+        ]);
 
         $response->assertStatus(200);
         $response->assertJson([
@@ -67,25 +81,135 @@ class VacationPlanTest extends TestCase
     /**
      * API show vacation plans endpoint test.
      */
-    public function test_api_show_vacation_plans(): void
+    public function test_api_show_all_vacation_plans(): void
     {
         
-        $this->create_personal_client();
-        $user = User::factory()->create();
-        $token = $user->createToken('token');
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
         /* $this->create_personal_client();
         $this->api_registration_endpoint_request();
         $token = $this->api_login_endpoint_request()->json()['Access_Token']; */
 
-        $response = $this->post('/api/vacationplans')->withHeaders([
+        VacationPlan::factory(5)->create();
+        
+        $response = $this->get('/api/vacationplans')->withHeaders([
             'Accept'=>'application/json',
             'Authorization'=>'Bearer '.$token->accessToken
         ]);
-        dd($response);
 
         $response->assertStatus(200);
         $response->assertJson([
             'Successful'=>true
         ]);
+    }
+
+    public function test_api_show_vacation_plan_by_id(): void
+    {
+        
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
+
+        VacationPlan::factory(5)->create();
+        
+        $response = $this->get('/api/vacationplans/1')->withHeaders([
+            'Accept'=>'application/json',
+            'Authorization'=>'Bearer '.$token->accessToken
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'Successful'=>true
+        ]);
+    }
+
+    public function test_api_create_vacation_plan(): void
+    {
+        
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
+
+        $response = $this->post('/api/vacationplans/',[
+            'title'=>'title 1',
+            'description'=>'description 1',
+            'date'=>'2024-06-12',
+            'location'=>'location 1',
+            'participants'=>'1',
+        ])->withHeaders([
+            'Accept'=>'application/json',
+            'Authorization'=>'Bearer '.$token->accessToken
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'Successful'=>true
+        ]);
+    }
+
+    public function test_api_update_vacation_plan(): void
+    {
+        
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
+
+        VacationPlan::factory(5)->create();
+        
+        $response = $this->put('/api/vacationplans/1',[
+            'title'=>'title 2',
+            'description'=>'description 2',
+            'date'=>'2024-06-12',
+            'location'=>'location 2',
+            'participants'=>'2',
+        ])->withHeaders([
+            'Accept'=>'application/json',
+            'Authorization'=>'Bearer '.$token->accessToken
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'Successful'=>true
+        ]);
+    }
+
+    public function test_api_delete_vacation_plan(): void
+    {
+        
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
+
+        VacationPlan::factory(5)->create();
+        
+        $response = $this->delete('/api/vacationplans/1')->withHeaders([
+            'Accept'=>'application/json',
+            'Authorization'=>'Bearer '.$token->accessToken
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'Successful'=>true
+        ]);
+    }
+
+    public function test_api_generate_pdf(): void
+    {
+        
+        $this->create_passport_personal_client();
+        
+        $token = $this->create_user_with_token();
+
+        VacationPlan::factory(5)->create();
+        
+        $this->get('/api/vacationplans/1/pdf')->withHeaders([
+            'Accept'=>'application/json',
+            'Authorization'=>'Bearer '.$token->accessToken
+        ]);
+
+        Pdf::assertRespondedWithPdf(function (PdfBuilder $pdf){
+            return str_contains($pdf->html, 'Your Vacation Plan');
+        });
     }
 }
